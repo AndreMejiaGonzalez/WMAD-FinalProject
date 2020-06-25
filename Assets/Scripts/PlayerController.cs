4a65e5dc4e397c5bc2a85fc1a59508cc06ac3f67
ANDREMEJIAGONZALEZ <andremejia64@gmail.com>
2020-06-24 20:59:37 -0700

    added Background effect, player prefab and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = movement * moveSpeed;
+    }
+}
