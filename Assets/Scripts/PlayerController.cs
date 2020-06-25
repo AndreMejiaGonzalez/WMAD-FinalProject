54f4af62278a924acdf9090b0add1258bfe1b3d5
ANDREMEJIAGONZALEZ <andremejia64@gmail.com>
2020-06-24 22:27:07 -0700

    added default shot prefab + shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Transform firePoint;
+    public GameObject projectile;
     public float moveSpeed;
+    public float fireRate;
+    public float fireCounter;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -18,7 +22,23 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        movementHandler();
+        FireHandler();
+    }
+
+    void movementHandler()
+    {
         Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         rb.velocity = movement * moveSpeed;
+    }
+
+    void FireHandler()
+    {
+        fireCounter -= Time.deltaTime;
+        if(fireCounter <= 0)
+        {
+            Instantiate(projectile, firePoint.position, new Quaternion(0,0,0,0));
+            fireCounter = fireRate;
+        }
     }
 }
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float shotSpeed;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb.velocity = this.transform.up * shotSpeed;
+    }
+}
